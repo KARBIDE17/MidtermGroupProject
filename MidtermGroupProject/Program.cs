6c5ce4e619b10bc406544941877a9bb7511c120f
KARBIDE17 <stevewoods17@gmail.com>
2023-03-08 21:59:55 -0500

    Added UI features

--- MidtermGroupProject/Program.cs
+++ MidtermGroupProject/Program.cs
@@ -15,19 +15,23 @@ List<Media> mediaList = fileio.FileRead();
 
 do
 {
+
     Console.WriteLine("Welcome to the Library!");
     Console.WriteLine("To see a list of available books, type \"List\" ");
     Console.WriteLine("To search for a book, type \"Search\" ");
     Console.WriteLine("To return a book, type \"Return\" ");
+    Console.WriteLine("To check out a book, type \"Check Out\" ");
+    Console.WriteLine();
 
     string input1 = Console.ReadLine().ToLower();
+    Console.WriteLine();
 
     if (input1 == "list")
     {
-        //display list of books using method
-        //List<Media> mediaList = fileio.FileRead();
+
         foreach (Book book in mediaList)
         {
+
             Console.WriteLine("Title: " + book.Title + ".  ");
             Console.WriteLine("Author: " + book.Author + ".  ");
             Console.WriteLine("Status: " + book.Status + ".  ");
@@ -36,31 +40,29 @@ do
             Console.WriteLine("Media Type:" + book.Type + ".  ");
             Console.WriteLine();
             Console.WriteLine();
+
         }
 
-        Console.WriteLine("Which book would you like to check out? Please enter the title");
-        string checkOutBook = Console.ReadLine();
-        List<Media> bookList = mediaList.Where(x => (x.Type == "Book") && (x.Status == "Available")).ToList();
-        Console.WriteLine(library.CheckOut(bookList, checkOutBook));
+
     }
     else if (input1 == "search")
     {
-        Console.WriteLine("Would you like to search by Title, or by Author?");
 
+        Console.WriteLine("Would you like to search by Title, or by Author?");
         string input2 = Console.ReadLine().ToLower();
 
         if (input2 == "title")
         {
+
             Console.WriteLine("Please enter the book title youd like to search for: ");
             string titleInput = Console.ReadLine();
             string title = titleInput.ToLower();
-
-            //List<Media> mediaList = fileio.FileRead();
 
             List<Media> searchResults = mediaList.Where(book => book.Title.ToLower().Contains(title)).ToList();
 
             foreach (Book book in searchResults)
             {
+
                 Console.WriteLine("Here are all books with the title: " + titleInput);
                 Console.WriteLine();
                 Console.WriteLine("Title: " + book.Title + ".  ");
@@ -71,20 +73,22 @@ do
                 Console.WriteLine("Media Type:" + book.Type + ".  ");
                 Console.WriteLine();
                 Console.WriteLine();
+
             }
+
         }
         else if (input2 == "author")
         {
+
             Console.WriteLine("Please enter the author youd like to search for: ");
             string authorInput = Console.ReadLine();
             string author = authorInput.ToLower();
-
-            //List<Media> mediaList = fileio.FileRead();
 
             List<Media> searchResults = mediaList.Where(book => book.Author.ToLower().Contains(author)).ToList();
 
             foreach (Book book in searchResults)
             {
+
                 Console.WriteLine("Here are all books with the title: " + author);
                 Console.WriteLine();
                 Console.WriteLine("Title: " + book.Title + ".  ");
@@ -95,85 +99,44 @@ do
                 Console.WriteLine("Media Type:" + book.Type + ".  ");
                 Console.WriteLine();
                 Console.WriteLine();
+
             }
         }
     }
     else if (input1 == "return")
     {
-        //Console.WriteLine("Here are the books currently checked out:");
-
-        //List<Media> returnResults = mediaList.Where(x => (x.Type == "Book") && (x.Status == "Checked Out")).ToList();
-
-        //foreach (var media in returnResults)
-        //{
-        //    Console.WriteLine("Title: " + media.Title + ".  ");
-        //    Console.WriteLine("Author: " + media.Author + ".  ");
-        //    Console.WriteLine("Status: " + media.Status + ".  ");
-        //    Console.WriteLine("Check Out Date" + media.CheckOutDate + ".  ");
-        //    Console.WriteLine("Due Date" + media.DueDate + ".  ");
-        //    Console.WriteLine("Media Type:" + media.Type + ".  \n\n");
-        //}
 
         Console.WriteLine("Which book are you returning? Please enter the title");
-        string returnBook = Console.ReadLine();
+        string returnBook = Console.ReadLine().ToLower();
         List<Media> bookList = mediaList.Where(x => x.Type == "Book").ToList();
         Console.WriteLine(library.CheckIn(bookList, returnBook));
+
+    }
+    else if (input1 == "check out")
+    {
+
+        Console.WriteLine("Which book would you like to check out? Please enter the title");
+        string checkOutBook = Console.ReadLine().ToLower();
+        List<Media> bookList = mediaList.Where(x => (x.Type == "Book") && (x.Status == "Available")).ToList();
+        Console.WriteLine(library.CheckOut(bookList, checkOutBook));
+
+    }
+    else if (string.IsNullOrWhiteSpace(input1))
+    {
+
+        Console.WriteLine("Sorry, please type in a selection from above.");
+
+    }
+    else
+    {
+        Console.WriteLine("Sorry that is not one of the choices.");
     }
 
-    Console.WriteLine("Would you like to try again? y/n?");
+    Console.WriteLine("Return to main menu? y/n?");
+    Console.WriteLine();
 
-} while (Console.ReadLine() == "y");
+} while (Console.ReadLine().ToLower() == "y");
+
+Console.WriteLine("Goodbye!");
 
 fileio.FileUpdate(mediaList);
-
-
-
-
-
-
-
-
-
-
-
-
-////remove a media item from the list
-////mediaList.Remove(mediaList.SingleOrDefault(x => x.Title == "The Dark Tower VII: The Dark Tower"));
-
-
-////add a book to the list
-//DateOnly DateOnly1 = new DateOnly(2023, 03, 04);
-//DateOnly DateOnly2 = new DateOnly(2023, 03, 11);
-//mediaList.Add(new Book("Book", "Available", DateOnly1, DateOnly2, "The Dark Tower VIII: Susannah's Revenge", "Stephen King"));
-
-
-////Update status of an item from the list, throws error if title is not unique
-//mediaList.Single(x => x.Title == "The Dark Tower VII: The Dark Tower").Status = "Checked Out";
-
-
-//wip-library_check_in/out
-
-
-
-
-
-////remove a media item from the list
-////mediaList.Remove(mediaList.SingleOrDefault(x => x.Title == "The Dark Tower VII: The Dark Tower"));
-
-
-////add a book to the list
-//DateOnly DateOnly1 = new DateOnly(2023, 03, 04);
-//DateOnly DateOnly2 = new DateOnly(2023, 03, 11);
-//mediaList.Add(new Book("Book", "Available", DateOnly1, DateOnly2, "The Dark Tower VIII: Susannah's Revenge", "Stephen King"));
-
-
-////Update status of an item from the list, throws error if title is not unique
-//mediaList.Single(x => x.Title == "The Dark Tower VII: The Dark Tower").Status = "Checked Out";
- //main
-
-//List<string> strings= new List<string>() { "Misery", "Firestarter", "Delores Claiborne" };
-
-//wip-library_check_in/out
-//=======
-//fileio.FileUpdate(mediaList);
-//main
