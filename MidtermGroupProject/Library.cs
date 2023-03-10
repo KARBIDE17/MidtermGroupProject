1c4130f9fc2db293f92166b7e74e8135eff9d0b8
Janda <SJanda@logility.com>
2023-03-09 21:55:54 -0500

    Revised fileIO and Library

--- MidtermGroupProject/Library.cs
+++ MidtermGroupProject/Library.cs
@@ -10,47 +10,27 @@ namespace MidtermGroupProject
     {
         public List<Book> Books { get; set; }
 
-        public List<Media> BookSearch(string searchCategory, string input, List<Media> mediaList)
-        {
-            List<Media> list = new List<Media>();
-            if (searchCategory.ToLower() == "author")
-            {
-                if (mediaList.Where(book => book.Author.ToLower().Contains(input)).ToList().Any())
-                {
-                    return mediaList.Where(book => book.Author.ToLower().Contains(input)).ToList();
-                }   
-            }
-            else
-            {
-                if (mediaList.Where(book => book.Title.ToLower().Contains(input)).ToList().Any())
-                {
-                    return mediaList.Where(book => book.Title.ToLower().Contains(input)).ToList();
-                }
-            }
-            return list; //possible to return an empty list
-        }
-
-        public string CheckOut(List<Media> mediaList, string title)
+        public string CheckOut(List<Media> mediaList, string title) //check out a book, returns message to user
         {
             foreach (var item in mediaList)
             {
                 if (item.Title.ToLower().Contains(title))
                 {
-                    if (item.Status == "Checked Out")
+                    if (item.Status == "Checked Out") //check if book is already checked out
                     {
                         return "This title is already checked out.";
                     }
 
                     mediaList.Single(x => x.Title == item.Title).Status = "Checked Out";
-                    mediaList.Single(x => x.Title == item.Title).DueDate = DateOnly.FromDateTime(DateTime.Now).AddDays(14);
-                    mediaList.Single(x => x.Title == item.Title).CheckOutDate = DateOnly.FromDateTime(DateTime.Now);
+                    mediaList.Single(x => x.Title == item.Title).DueDate = DateOnly.FromDateTime(DateTime.Now).AddDays(14); //2 weeks
+                    mediaList.Single(x => x.Title == item.Title).CheckOutDate = DateOnly.FromDateTime(DateTime.Now); //today
                     return $"Thank you checking out {item.Title}!";
                 }
             }
             return "I dont recognize that title.";
         }
 
-        public string CheckIn(List<Media> mediaList,string title)
+        public string CheckIn(List<Media> mediaList,string title) //check in a book, returns message to user
         {
 
             foreach (var item in mediaList)
@@ -58,24 +38,18 @@ namespace MidtermGroupProject
                 if (item.Title.ToLower().Contains(title))
 
                 {
-                    if (item.Status == "Available")
+                    if (item.Status == "Available") //check if book is already checked in
                     {
                         return "This title is already checked in.";
                     }
 
                     mediaList.Single(x => x.Title == item.Title).Status = "Available";
-                    mediaList.Single(x => x.Title == item.Title).DueDate = null;
-                    mediaList.Single(x => x.Title == item.Title).CheckOutDate = null;
+                    mediaList.Single(x => x.Title == item.Title).DueDate = null; //removes due date
+                    mediaList.Single(x => x.Title == item.Title).CheckOutDate = null; //removes check out date
                     return $"Thank you for returning {item.Title}!";
                 }
             }
             return "I dont recognize that title.";
-        }
-
-        public List<Media> Collection(string type)
-        {
-            List<Media> list = new List<Media>();
-            return list.Where(x => x.Type.ToLower() == type).ToList();
         }
     }
 }
