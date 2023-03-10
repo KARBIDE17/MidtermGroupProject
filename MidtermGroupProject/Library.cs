5c3c2e7ae447603b1f57d1e5184acb301b9b20ea
Janda <SJanda@logility.com>
2023-03-09 19:55:38 -0500

    final branch commit

--- MidtermGroupProject/Library.cs
+++ MidtermGroupProject/Library.cs
@@ -36,34 +36,38 @@ namespace MidtermGroupProject
             {
                 if (item.Title.ToLower().Contains(title))
                 {
+                    if (item.Status == "Checked Out")
+                    {
+                        return "This title is already checked out.";
+                    }
+
                     mediaList.Single(x => x.Title == item.Title).Status = "Checked Out";
                     mediaList.Single(x => x.Title == item.Title).DueDate = DateOnly.FromDateTime(DateTime.Now).AddDays(14);
                     mediaList.Single(x => x.Title == item.Title).CheckOutDate = DateOnly.FromDateTime(DateTime.Now);
                     return $"Thank you checking out {item.Title}!";
                 }
             }
-            if (mediaList.Single(x => (x.Title.ToLower().Contains(title)) && (x.Status == "Checked Out")) is not null)
-            {
-                return "This title is already checked out.";
-            }
             return "I dont recognize that title.";
         }
 
         public string CheckIn(List<Media> mediaList,string title)
         {
+
             foreach (var item in mediaList)
             {
                 if (item.Title.ToLower().Contains(title))
+
                 {
+                    if (item.Status == "Available")
+                    {
+                        return "This title is already checked in.";
+                    }
+
                     mediaList.Single(x => x.Title == item.Title).Status = "Available";
                     mediaList.Single(x => x.Title == item.Title).DueDate = null;
                     mediaList.Single(x => x.Title == item.Title).CheckOutDate = null;
                     return $"Thank you for returning {item.Title}!";
                 }
-            }
-            if (mediaList.Single(x => (x.Title.ToLower().Contains(title)) && (x.Status == "Checked In")) is not null)
-            {
-                return "This title is already checked in.";
             }
             return "I dont recognize that title.";
         }
@@ -71,7 +75,7 @@ namespace MidtermGroupProject
         public List<Media> Collection(string type)
         {
             List<Media> list = new List<Media>();
-            return list.Where(x => x.Type.ToLower() == type).ToList(); //can return empty list
+            return list.Where(x => x.Type.ToLower() == type).ToList();
         }
     }
 }
